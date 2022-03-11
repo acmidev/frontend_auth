0b408233f35aa3f0cbc2147315ed13fe9e4d2eb3
acmidev <mharmel.ach@gmail.com>
2022-03-11 19:02:12 +0100

    register modal done

--- main.js
+++ main.js
@@ -4,24 +4,47 @@ var password = document.getElementById('password')
 var repeatPassword = document.getElementById('repeat-password')
 var register = document.getElementById('register')
 var error = document.getElementById('error')
+var registerContainer = document.getElementById('register-container')
+var loginContainer = document.getElementById('login-container')
 
 
 register.addEventListener('click', saveData)
 
 
 function saveData() {
+    const user = JSON.parse(window.localStorage.getItem('user'))
+    
 
-    if(!email.value =="" && !password.value=="" && !repeatPassword ==""){
-        if(password.value === repeatPassword.value){
-            
-            console.log(repeatPassword.value)
-
-
-
-        }else{error.innerText = "Passwords not identical"}
+    if(user.email === email.value){
+        error.innerText = "Account already exist"
     }else {
-        error.innerText = "Complete all the fields"
-        console.log('error complete all the fields')
+        if(!email.value =="" && !password.value=="" && !repeatPassword ==""){
+            if(password.value === repeatPassword.value){
+                
+                const user = {
+                    "email": email.value,
+                    "password": password.value
+                    
+                }
+                window.localStorage.setItem('user', JSON.stringify(user))
+    
+    
+                console.log(repeatPassword.value)
+    
+                if(user.email && user.password){
+
+                    
+                    registerContainer.style.display = "none"
+                    loginContainer.style.display = "grid"
+                }
+    
+            }else{error.innerText = "Passwords not identical"}
+        }else {
+            error.innerText = "Complete all the fields"
+            console.log('error complete all the fields')
+        }
+    
+    }
     }
 
-}
+    
